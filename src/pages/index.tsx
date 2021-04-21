3d45c58edf87aefab806586bf61fde0073905580
Ricardo Mariz <shaaad@gmail.com>
2021-04-21 10:41:58 -0300

    added axios to project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,41 @@
-export default function Home() {
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <h1>index</h1>
+    <div>
+      <h1>index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
 
   )
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('/episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
